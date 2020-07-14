09b3138ce4fb75adcbaa884c4c8e6669dc47917d
Edmond <edmond.b@hotmail.co.uk>
2020-07-14 17:16:13 +0100

    Changed logo and link

--- src/App.js
+++ src/App.js
@@ -6,18 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'eb-red.png'} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edmond Bullaj
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://github.com/edmond-b" target="_blank" rel="noopener noreferrer">GitHub</a>
       </header>
     </div>
   );
