3e48240636a8747694599e172bb2892a503f0acb
Edmond <edmond.b@hotmail.co.uk>
2020-10-21 13:18:14 +0100

    Maintenance

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={'icons/eb-red.png'} className="App-logo" alt="logo" />
         <p>
           Edmond Bullaj
+          Under Maintenance
+          Back Soon
         </p>
         <a className="App-link" href="https://github.com/edmond-b" target="_blank" rel="noopener noreferrer">GitHub</a>
       </header>
