2252ae93cd1bb3567a69a960ff49d2a01a8559bd
Edmond <edmond.b@hotmail.co.uk>
2020-11-24 17:23:45 +0000

    Navbar home link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-      <h1>Hello World!</h1>
-
-      <header className="App-header">
-        <img src={'icons/eb-red.png'} className="App-logo" alt="logo" />
-        <p>
-          Under Maintenance
-        </p>
-        <p>
-          Back Soon
-        </p>
-        <a className="App-link" href="https://github.com/edmond-b" target="_blank" rel="noopener noreferrer">GitHub</a>
-      </header>
-
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
       <nav className="navbar">
-
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            EB <img src={'icons/eb-red.png'} className="App-logo" alt="logo" />
+          </Link>
+        </div>
       </nav>
     </>
   )
 }
 
-export defults Navbar
+export default Navbar;
