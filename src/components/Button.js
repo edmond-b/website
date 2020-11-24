1b93c2407736039892d3e32dbef2c1df844c8cf2
Edmond <edmond.b@hotmail.co.uk>
2020-11-24 18:46:19 +0000

    NavBar and button css added

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,12 +9,14 @@ const SIZES = ['btn--medium', 'btn--large'];
 export const Button = ({
   children,
   type,
-  onclick,
+  onClick,
   buttonStyle,
   buttonSize
 }) => {
-  const checkButtonStyle = STYLES.incules(buttonStyle) ? buttonStyle : STYLES[0];
-  const checkButtonSize = SIZES.incules(buttonSize) ? buttonSize : SIZES[0];
+  const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
   return (
     <Link to='/sign-up' className='btn-mobile'>
       <button
@@ -25,5 +27,5 @@ export const Button = ({
         {children}
       </button>
     </Link>
-  )
+  );
 };
