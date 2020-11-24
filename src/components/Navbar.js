da9505fa9fe7b62572131e46a2a23ac84d06ae6d
Edmond <edmond.b@hotmail.co.uk>
2020-11-24 19:39:12 +0000

    navbar done no logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            <img src={'icons/eb-red-small.png'} className="App-logo" alt="logo" />
+            EB
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
