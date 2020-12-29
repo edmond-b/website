e9ce352e1b874b1df9a1ef576103b93ed2671568
Edmond <edmond.b@hotmail.co.uk>
2020-12-29 12:01:16 +0000

    app.js returned to working state

--- src/App.js
+++ src/App.js
@@ -8,24 +8,21 @@ import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 import Placeholder from './components/pages/Placeholder';
 
-
 function App() {
-  render() {
-    return (
-      <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/info' component={Info}/>
-          <Route path='/projects' component={Projects}/>
-          <Route path='/contact' component={Contact}/>
-          <Route path='/placeholder' component={Placeholder}/>
-        </Switch>
-      </Router>
-      </>
-    );
-  }
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/info' component={Info}/>
+        <Route path='/projects' component={Projects}/>
+        <Route path='/contact' component={Contact}/>
+        <Route path='/placeholder' component={Placeholder}/>
+      </Switch>
+    </Router>
+    </>
+  );
 }
 
 export default App;
